5eeded5b2ed20cb7fa1a396c163fab1392247fec
Dejan Gitin <dejangitin@gmail.com>
2024-08-23 21:13:40 -0400

    fix

--- src/app.rs
+++ src/app.rs
@@ -141,7 +141,8 @@ impl MealPlannerApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-        let default_state = serde_json::from_slice(base64::decode(include_bytes!("../state.json").unwrap().expect("Error decoding."))).unwrap();
+        let json = base64::decode(include_bytes!("../state.json")).unwrap();
+        let default_state = serde_json::from_slice(json.as_slice()).unwrap();
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
