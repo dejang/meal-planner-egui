f54846e3980a3335e8a9e3deb5f8256d7f197379
dejang <garret2k2@gmail.com>
2025-02-09 23:17:37 -0500

    fix toolchain reqs

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'egui-template-pwa';
 var filesToCache = [
-  './',
-  './index.html',
-  './meal_planner.js',
-  './meal_planner_bg.wasm',
+  // './',
+  // './index.html',
+  // './meal_planner.js',
+  // './meal_planner_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
